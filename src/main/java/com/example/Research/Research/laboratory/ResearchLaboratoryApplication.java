7e2db7b16548cf137d8af97075e11d822d6ce192
Souguir Oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2022-06-16 20:45:00 +0100

    first commit

---
+++ src/main/java/com/example/Research/Research/laboratory/ResearchLaboratoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.Research.Research.laboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchLaboratoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchLaboratoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Research/Research/laboratory/ResearchLaboratoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Research.Research.laboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchLaboratoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
